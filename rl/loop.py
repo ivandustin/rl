a2c90be56846c1c961ff372a978c1029fd677a68
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-05-13 16:30:47 +0800

    Simplify loop

--- rl/loop.py
+++ rl/loop.py
@@ -1,14 +1,14 @@
 from .exceptions import No, Yes
 
 
-def loop(module):
-    state = module.init()
-    while not module.stop(state):
-        action = module.ask(state)
-        next = module.apply(action, state)
-        if module.no(action, state, next):
+def loop(init, stop, ask, apply, no, yes):
+    state = init()
+    while not stop(state):
+        action = ask(state)
+        next = apply(action, state)
+        if no(action, state, next):
             raise No(state, action)
-        if module.yes(action, state, next):
+        if yes(action, state, next):
             raise Yes(state, action)
         yield state, action
         state = next
